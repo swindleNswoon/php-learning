5b0f7c034c687d99b48bd3dcdca9ac19c48dcae2
Brian <bijil6339@gmail.com>
2017-04-26 22:42:04 -0400

    Intro to pdo's

--- index.php
+++ index.php
@@ -1,41 +1,13 @@
 <?php
 require 'functions.php';
+require 'task.php';
 
-class Task 
-{
-	public $description;
-	public $completed = false;
-	
-	public function __construct($description) {
-		$this->description = $description;
-	}
-	public function complete() {
-		$this->completed = true;
-	}
+$pdo=connectToDb();
+$tasks = fetchAllTasks($pdo);
 
-	public function isComplete() {
-		return $this->completed;
-	}
 
-	
-}
 
-$tasks = [
-	new Task('Go to the store'),
-	new Task('Walk Dogs'),
-	new Task('Vacuumn'),
-	new Task('Make Dinner')
-];
 
-$tasks[0]->complete();
-
-//$task = new Task('Go to the store'); //create task
-
-//$task->complete(); //complete task
-
-//var_dump($task->isComplete());
-
-//testing that git pull worked
 	
 require 'index.view.php';
 ?>
